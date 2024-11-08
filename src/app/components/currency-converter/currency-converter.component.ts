b4381c98fb57bbafbd75b5a6bec079bf1a4d6d22
AniIlya <116735960+AniIlya@users.noreply.github.com>
2024-11-08 11:46:59 +0500

    add localization variable to localStorag

--- src/app/components/currency-converter/currency-converter.component.ts
+++ src/app/components/currency-converter/currency-converter.component.ts
@@ -23,10 +23,191 @@ interface ExchangeRatesByMode {
   templateUrl: './currency-converter.component.html',
   styleUrls: ['./currency-converter.component.scss']
 })
+
+
+// export class CurrencyConverterComponent implements OnInit {
+//   amount: number = 0;
+//   fromCurrency: string = 'TJS'; // Сомони по умолчанию
+//   toCurrency: string = 'USD'; // Валюта для конвертации по умолчанию
+//   convertedAmount: number = 0;
+//   lastUpdated: string = '';
+//   selectedMode: string = ''; // Выбранный режим обмена
+//   exchangeRatesByMode: ExchangeRatesByMode = {}; // Хранение курсов по режимам
+//   showMore: boolean = false;
+  
+//   transactionType: string = 'buy';
+
+//   constructor(private http: HttpClient) {}
+
+//   ngOnInit() {
+//     this.fetchExchangeRates(); // Загружаем курсы при инициализации компонента
+//   }
+//   get ratesCount(): number {
+//     return this.exchangeRatesByMode[this.selectedMode]?.length || 0;
+//   }
+//   // Получение курсов обмена с сервера
+//   fetchExchangeRates() {
+//     this.http.get('http://192.168.42.200:8025/ws/v3/info/exchange-rates', { responseType: 'text' })
+//       .subscribe({
+//         next: (data: string) => {
+//           this.parseXML(data); // Парсим XML, если запрос успешен
+//         },
+//         error: (err) => {
+//           console.error('Error fetching exchange rates:', err); // Обработка ошибок
+//         }
+//       });
+//   }
+
+//   // Парсинг XML данных
+//   parseXML(data: string) {
+//     const parser = new DOMParser();
+//     const xml = parser.parseFromString(data, 'application/xml');
+//     const rates = xml.getElementsByTagName('rate');
+//     this.exchangeRatesByMode = {}; // Очистка предыдущих данных
+
+//     Array.from(rates).forEach(rate => {
+//       const mode = rate.getElementsByTagName('mode')[0].textContent || ''; // Режим обмена
+//       const currency = rate.getElementsByTagName('cur')[0].textContent || ''; // Валюта
+//       const buy = parseFloat(rate.getElementsByTagName('buy')[0].textContent || '0'); // Покупка
+//       const sell = parseFloat(rate.getElementsByTagName('sell')[0].textContent || '0'); // Продажа
+//       const flag = this.getFlag(currency); // Получение иконки флага для валюты
+
+//       if (!this.exchangeRatesByMode[mode]) {
+//         this.exchangeRatesByMode[mode] = [];
+//       }
+
+//       this.exchangeRatesByMode[mode].push({ currency, buy, sell, flag });
+//     });
+
+//     this.lastUpdated = xml.getElementsByTagName('lastUpdate')[0]?.textContent || ''; // Получение даты последнего обновления
+
+//     const modes = this.getModes();
+//     if (modes.length > 0) {
+//       this.selectedMode = modes[0]; // Установка режима по умолчанию
+//       this.updateCurrencies(); // Обновляем валюты после выбора режима
+//     }
+//   }
+
+//   // Логика получения пути к флагу по коду валюты
+//   getFlag(currency: string): string {
+//     switch (currency) {
+//       case 'USD': return '../../assets/icons/usd.svg';
+//       case 'EUR': return '../../assets/icons/euro.svg';
+//       case 'RUB': return '../../assets/icons/rub.svg';
+//       case 'KZT': return '../../../assets/icons/kzt.png';
+//       case 'TJS': return '../../assets/icons/tjs.svg';
+//       default: return '../../assets/icons/default.svg'; // Значение по умолчанию
+//     }
+//   }
+
+//   // Функция для преобразования режимов в удобочитаемые строки
+//   getReadableMode(mode: string): string {
+//     switch (mode) {
+//       case 'CASH': return 'В кассе';
+//       case 'REMITTANCE_RATE': return 'Денежные переводы';
+//       case 'CB_RATE': return 'НБТ';
+//       default: return mode;
+//     }
+//   }
+
+//   // Функция для получения списка режимов
+//   getModes(): string[] {
+//     return ['REMITTANCE_RATE', 'CASH', 'CB_RATE']; // Возвращаем только нужные режимы
+//   }
+
+//   // Функция для выбора режима
+//   selectMode(mode: string) {
+//     this.selectedMode = mode;
+//     this.updateCurrencies(); // Обновляем валюты после выбора режима
+//   }
+
+//   // Обновляем валюты после выбора режима
+//   updateCurrencies() {
+//     if (this.selectedMode) {
+//       const rates = this.exchangeRatesByMode[this.selectedMode]; // Получаем курсы для выбранного режима
+//       if (rates.length > 0) {
+//         // Убедимся, что при переключении режима валюты корректно выбираются
+//         if (this.transactionType === 'buy') {
+//           this.fromCurrency = 'TJS';
+//           this.toCurrency = rates.find(rate => rate.currency !== 'TJS')?.currency || rates[0].currency;
+//         } else {
+//           this.fromCurrency = rates.find(rate => rate.currency !== 'TJS')?.currency || rates[0].currency;
+//           this.toCurrency = 'TJS';
+//         }
+//         this.convertCurrency(); // Выполняем конвертацию
+//       }
+//     }
+//   }
+
+//   // Функция для конвертации валют
+//   setTransactionType(type: string) {
+//     this.transactionType = type;
+//     this.updateCurrencySelection(); // Обновляем выбор валют после изменения типа транзакции
+//     this.convertCurrency();
+//   }
+
+//   // Метод для переключения состояния при каждом клике
+//   toggleTransaction(): void {
+//     this.transactionType = this.transactionType === 'buy' ? 'sell' : 'buy';
+//     this.updateCurrencySelection(); // Обновляем выбор валют после переключения типа транзакции
+//     this.convertCurrency();
+
+//     // Запуск анимации
+//     const anim = document.querySelector('animate');
+//     if (anim) {
+//       (anim as any).beginElement(); // Запуск анимации
+//     }
+//   }
+
+//   // Обновление выбора валют для поддержания корректного обмена
+//   updateCurrencySelection() {
+//     if (this.transactionType === 'buy') {
+//       // Если покупаем, выбираем любую валюту для покупки, но платим Сомони
+//       this.fromCurrency = 'TJS';
+//       const rates = this.exchangeRatesByMode[this.selectedMode];
+//       this.toCurrency = rates.find(rate => rate.currency !== 'TJS')?.currency || '';
+//     } else {
+//       // Если продаем, выбираем любую валюту для продажи и получаем Сомони
+//       const rates = this.exchangeRatesByMode[this.selectedMode];
+//       this.fromCurrency = rates.find(rate => rate.currency !== 'TJS')?.currency || '';
+//       this.toCurrency = 'TJS';
+//     }
+//   }
+
+//   // Функция для конвертации валют
+//   convertCurrency() {
+//     if (this.transactionType === 'buy') {
+//       // Покупаем валюту за сомони
+//       const toRate = this.exchangeRatesByMode[this.selectedMode]?.find(rate => rate.currency === this.toCurrency);
+//       if (toRate) {
+//         this.convertedAmount = this.amount / toRate.sell;
+//       } else {
+//         this.convertedAmount = 0; // Если курсы не найдены
+//       }
+//     } else {
+//       // Продаем валюту и получаем сомони
+//       const fromRate = this.exchangeRatesByMode[this.selectedMode]?.find(rate => rate.currency === this.fromCurrency);
+//       if (fromRate) {
+//         this.convertedAmount = this.amount * fromRate.buy;
+//       } else {
+//         this.convertedAmount = 0; // Если курсы не найдены
+//       }
+//     }
+//   }
+
+//   toggleShowMore() {
+//     this.showMore = !this.showMore;
+//   }
+
+//   // Отправка денег (эмуляция)
+//   sendMoney() {
+//     alert(`Вы отправили ${this.convertedAmount.toFixed(2)} ${this.toCurrency}`);
+//   }
+// }
 export class CurrencyConverterComponent implements OnInit {
   amount: number = 0;
-  fromCurrency: string = '';
-  toCurrency: string = '';
+  fromCurrency: string = 'TJS'; // Сомони по умолчанию
+  toCurrency: string = 'USD'; // Валюта для конвертации по умолчанию
   convertedAmount: number = 0;
   lastUpdated: string = '';
   selectedMode: string = ''; // Выбранный режим обмена
@@ -93,6 +274,7 @@ export class CurrencyConverterComponent implements OnInit {
       case 'EUR': return '../../assets/icons/euro.svg';
       case 'RUB': return '../../assets/icons/rub.svg';
       case 'KZT': return '../../../assets/icons/kzt.png';
+      case 'TJS': return '../../assets/icons/tjs.svg';
       default: return '../../assets/icons/default.svg'; // Значение по умолчанию
     }
   }
@@ -109,7 +291,7 @@ export class CurrencyConverterComponent implements OnInit {
 
   // Функция для получения списка режимов
   getModes(): string[] {
-    return [  'CB_RATE','REMITTANCE_RATE','CASH']; // Возвращаем только нужные режимы
+    return ['REMITTANCE_RATE', 'CASH', 'CB_RATE']; // Возвращаем только нужные режимы
   }
 
   // Функция для выбора режима
@@ -121,16 +303,17 @@ export class CurrencyConverterComponent implements OnInit {
   // Обновляем валюты после выбора режима
   updateCurrencies() {
     if (this.selectedMode) {
-      const originalMode = Object.keys(this.exchangeRatesByMode).find(key => this.getReadableMode(key) === this.selectedMode);
-      if (originalMode) {
-        const rates = this.exchangeRatesByMode[originalMode]; // Получаем курсы для выбранного режима
-        if (rates.length > 0) {
-          this.fromCurrency = rates[0].currency; // Устанавливаем начальную валюту
-          this.toCurrency = rates[0].currency !== this.fromCurrency ? rates[0].currency : (rates[1] ? rates[1].currency : rates[0].currency); // Устанавливаем валюту для конвертации
-          this.convertCurrency(); // Выполняем конвертацию
+      const rates = this.exchangeRatesByMode[this.selectedMode]; // Получаем курсы для выбранного режима
+      if (rates.length > 0) {
+        // Убедимся, что при переключении режима валюты корректно выбираются
+        if (this.transactionType === 'buy') {
+          this.fromCurrency = 'TJS';
+          this.toCurrency = rates.find(rate => rate.currency !== 'TJS')?.currency || rates[0].currency;
+        } else {
+          this.fromCurrency = rates.find(rate => rate.currency !== 'TJS')?.currency || rates[0].currency;
+          this.toCurrency = 'TJS';
         }
-      } else {
-        console.error('Original mode not found');
+        this.convertCurrency(); // Выполняем конвертацию
       }
     }
   }
@@ -138,32 +321,61 @@ export class CurrencyConverterComponent implements OnInit {
   // Функция для конвертации валют
   setTransactionType(type: string) {
     this.transactionType = type;
+    this.updateCurrencySelection(); // Обновляем выбор валют после изменения типа транзакции
     this.convertCurrency();
   }
- // Метод для переключения состояния при каждом клике
- toggleTransaction(): void {
-  // Если текущее состояние "Купить", переключаем на "Продать", и наоборот
-  this.transactionType = this.transactionType === 'buy' ? 'sell' : 'buy';
 
-  // Запуск анимации
-  const anim = document.querySelector('animate');
-  if (anim) {
-    (anim as any).beginElement(); // Запуск анимации
+  // Метод для переключения состояния при каждом клике
+  toggleTransaction(): void {
+    this.transactionType = this.transactionType === 'buy' ? 'sell' : 'buy';
+    this.updateCurrencySelection(); // Обновляем выбор валют после переключения типа транзакции
+    this.convertCurrency();
+
+    // Запуск анимации
+    const anim = document.querySelector('animate');
+    if (anim) {
+      (anim as any).beginElement(); // Запуск анимации
+    }
   }
-}
+
+  // Обновление выбора валют для поддержания корректного обмена
+  updateCurrencySelection() {
+    if (this.transactionType === 'buy') {
+      // Если покупаем, выбираем любую валюту для покупки, но платим Сомони
+      this.fromCurrency = 'TJS';
+      const rates = this.exchangeRatesByMode[this.selectedMode];
+      this.toCurrency = rates.find(rate => rate.currency !== 'TJS')?.currency || '';
+    } else {
+      // Если продаем, выбираем любую валюту для продажи и получаем Сомони
+      const rates = this.exchangeRatesByMode[this.selectedMode];
+      this.fromCurrency = rates.find(rate => rate.currency !== 'TJS')?.currency || '';
+      this.toCurrency = 'TJS';
+    }
+  }
+
   // Функция для конвертации валют
   convertCurrency() {
-    const fromRate = this.exchangeRatesByMode[this.selectedMode]?.find(rate => rate.currency === this.fromCurrency);
-    const toRate = this.exchangeRatesByMode[this.selectedMode]?.find(rate => rate.currency === this.toCurrency);
+    if (this.fromCurrency === this.toCurrency) {
+      this.convertedAmount = 0; // Нельзя конвертировать одну и ту же валюту
+      return;
+    }
 
-    if (fromRate && toRate) {
-      if (this.transactionType === 'buy') {
-        this.convertedAmount = (this.amount * fromRate.sell) / toRate.buy;
+    if (this.transactionType === 'buy') {
+      // Покупаем валюту за сомони
+      const toRate = this.exchangeRatesByMode[this.selectedMode]?.find(rate => rate.currency === this.toCurrency);
+      if (toRate) {
+        this.convertedAmount = this.amount / toRate.sell;
       } else {
-        this.convertedAmount = (this.amount * fromRate.buy) / toRate.sell;
+        this.convertedAmount = 0; // Если курсы не найдены
       }
     } else {
-      this.convertedAmount = 0; // Если курсы не найдены
+      // Продаем валюту и получаем сомони
+      const fromRate = this.exchangeRatesByMode[this.selectedMode]?.find(rate => rate.currency === this.fromCurrency);
+      if (fromRate) {
+        this.convertedAmount = this.amount * fromRate.buy;
+      } else {
+        this.convertedAmount = 0; // Если курсы не найдены
+      }
     }
   }
 
@@ -174,4 +386,5 @@ export class CurrencyConverterComponent implements OnInit {
   // Отправка денег (эмуляция)
   sendMoney() {
     alert(`Вы отправили ${this.convertedAmount.toFixed(2)} ${this.toCurrency}`);
-  }}
+  }
+}
--- src/app/languages.service.ts
+++ src/app/languages.service.ts
@@ -5,15 +5,22 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class LanguagesService {
-  private language = new BehaviorSubject<string>('1');  // Default language is Tajik
-
-  // Observable for watching language changes
-  public language$ = this.language.asObservable();
-
-  constructor() {}
-
-  setLanguage(lang: string) {
-    console.log(lang)
-    this.language.next(lang);
+    private language = new BehaviorSubject<string>('1');  // Default language is Tajik
+  
+    // Observable for watching language changes
+    public language$ = this.language.asObservable();
+  
+    constructor() {
+      // Load saved language from localStorage if available
+      const savedLanguage = localStorage.getItem('appLanguage');
+      if (savedLanguage) {
+        this.language.next(savedLanguage);
+      }
+    }
+  
+    setLanguage(lang: string) {
+      console.log(lang);
+      this.language.next(lang); // Update the current language
+      localStorage.setItem('appLanguage', lang); // Save the language to localStorage
+    }
   }
-}
