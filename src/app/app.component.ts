f5539c7eb3a30dfc822519c4451fcc418c809879
Muhiddin02 <sirojiddinovm98@gmail.com>
2025-10-27 14:13:43 +0500

    removed not needed code from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
-// import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ModalComponent } from './components/modal/modal.component';
-import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators'; // Импортируем оператор filter
+import { RouterOutlet } from '@angular/router';
 import { PageTitleService } from './services/page-title.service';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { Languages } from './shared/enums/languages.enum';
@@ -18,21 +16,11 @@ import { Languages } from './shared/enums/languages.enum';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  private router = inject(Router);
   private translate = inject(TranslateService);
   private pageTitleService = inject(PageTitleService);
 
-  constructor() {
-    this.router.events
-      .pipe(
-        // 3. Нас интересует только событие NavigationEnd (успешное завершение навигации)
-        filter((event): event is NavigationEnd => event instanceof NavigationEnd),
-      )
-      .subscribe(() => {
-        // 4. При каждом успешном переходе прокручиваем окно на самый верх
-        window.scrollTo(0, 0);
-      });
-  }
+  constructor() {}
+
   ngOnInit(): void {
     this.pageTitleService.init();
     const savedLanguage = localStorage.getItem('appLanguage');
